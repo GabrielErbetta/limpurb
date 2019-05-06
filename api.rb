ba07c241a857b802a28a900c65e09363bcf5d5af
Gabriel Erbetta <gabrielerbetta@gmail.com>
2019-05-06 19:00:40 -0300

    Changed api to process requests in background

--- api.rb
+++ api.rb
@@ -12,36 +12,25 @@ get '/status' do
   'ok'
 end
 
-# Sweeps the git repo
+# Sweeps the pull request for linter warnings
 post '/sweep' do
   payload = json_params
-  halt 200 unless %w[opened synchronize].include? payload['action']
 
   repo_owner = payload['repository']['owner']['login']
   repo_name  = payload['repository']['name']
   pr_number  = payload['number']
   pr_base    = payload['pull_request']['base']['ref']
 
-  repo = load_git_repo repo_owner, repo_name
-
-  if payload['action'] == 'opened'
-    # fetch branch
-    # checkout to branch
-    # run pronto to master
-
-    repo.fetch('origin')
-    repo.branch("origin/#{pr_base}").checkout
-    repo.branch("pr-#{pr_number}").checkout
-    repo.pull('origin', "pull/#{pr_number}/head")
-
-    ENV['PRONTO_PULL_REQUEST_ID'] = pr_number
-    Pronto.run(pr_base, "repos/#{repo_owner}/#{repo_name}", pronto_formatters)
-  elsif payload['action'] == 'synchronize'
-    # save current HEAD
-    # checkout to branch
-    # update branch
-    # run pronto to HEAD
+  case payload['action']
+  when 'opened'
+    Thread.new do
+      process_opened_pr(repo_owner, repo_name, pr_number, pr_base)
+    end
+  when 'synchronize'
+    Thread.new do
+      process_synchronize_pr repo_owner, repo_name, pr_number, pr_base
+    end
   end
 
-  payload.inspect
+  halt 200
 end
--- helpers.rb
+++ helpers.rb
@@ -6,6 +6,12 @@ rescue JSON::ParserError
   halt 500, { message: 'Invalid JSON' }.to_json
 end
 
+def pronto_formatters
+  formatter = Pronto::Formatter::GithubPullRequestReviewFormatter.new
+  status_formatter = Pronto::Formatter::GithubStatusFormatter.new
+  [formatter, status_formatter]
+end
+
 def load_git_repo(r_owner, r_name)
   File.new("#{settings.root}/log/#{r_owner}_#{r_name}.log", 'a+')
   log = Logger.new("#{settings.root}/log/#{r_owner}_#{r_name}.log")
@@ -21,8 +27,25 @@ def load_git_repo(r_owner, r_name)
   repo
 end
 
-def pronto_formatters
-  formatter = Pronto::Formatter::GithubPullRequestReviewFormatter.new
-  status_formatter = Pronto::Formatter::GithubStatusFormatter.new
-  [formatter, status_formatter]
+def process_opened_pr(repo_owner, repo_name, pr_number, pr_base)
+  repo = load_git_repo repo_owner, repo_name
+
+  repo.branch("pr-#{pr_number}-base").delete rescue nil
+  repo.branch("pr-#{pr_number}").delete rescue nil
+
+  repo.fetch('origin', ref: "#{pr_base}:pr-#{pr_number}-base")
+  repo.fetch('origin', ref: "pull/#{pr_number}/head:pr-#{pr_number}")
+  repo.branch("pr-#{pr_number}").checkout
+
+  ENV['PRONTO_PULL_REQUEST_ID'] = pr_number.to_s
+  Pronto.run(pr_base, "repos/#{repo_owner}/#{repo_name}", pronto_formatters)
+end
+
+def process_synchronize_pr(repo_owner, repo_name, pr_number, pr_base)
+  # TODO
+
+  # save current HEAD
+  # checkout to branch
+  # update branch
+  # run pronto to HEAD
 end
